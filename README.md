a51999099efd36ac631c5cda49f39e09bf9db372
udutharaju <rajuudutha9963@gmail.com>
2025-06-01 14:51:01 +0530

    log and exception

---
+++ README.md
@@ -0,0 +1,1 @@
+## network security projecct for phishing data
--- setup.py
+++ setup.py
@@ -1,1 +1,30 @@
 # setup.py contains all general info about the versions and all
+
+from setup import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT ='-e .'
+
+def get_requirements(file_path:str)->list[str]:
+    '''
+    this function will return list of requirements
+    '''
+    requirements =[]
+    with open(file_path) as file_obj:
+        requirements = file_path.readlines()
+        requirements=[req.replace('\n','') for req  in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+setup(
+    name = 'networksecurity'
+    version = '0.0.1',
+    author = 'raju',
+    author_email = 'rajuudutha9963@gmail.com'
+    packages= find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+    
+)
+
