c0ccc7f3b794f0a272ee9128b1d048a90b77a093
udutharaju <rajuudutha9963@gmail.com>
2025-06-03 17:25:41 +0530

    data_validation

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig 
-from networksecurity.entity.config_entity import Training_pipeline_config 
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig 
+from networksecurity.components.data_validation import DataValidation
 import sys
 
 if __name__ == "__main__":
     
     try:
-        trainingpipelineconfig =Training_pipeline_config()
+        trainingpipelineconfig =TrainingPipelineConfig()
         dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("initiate the data ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
-
+        logging.info("Data initiation completed")
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("initiate data validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data validation completed")
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -94,9 +94,6 @@ class DataIngestion:
             dataframe = self.export_collection_as_dataframe()
             dataframe = self.export_dat_into_feature_store(dataframe)
             self.split_data_as_train_test(dataframe)
-            # dataframe=self.export_collection_as_dataframe()
-            # dataframe=self.export_data_into_feature_store(dataframe)
-            # self.split_data_as_train_test(dataframe)
             dataingestionartifact = DataIngestionArtifact(trained_file_path = self.data_ingestion_config.training_file_path,
                                                           test_file_path = self.data_ingestion_config.testing_file_path
                                                           )
